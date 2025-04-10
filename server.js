d80a983d1bdabe95c5e094ba2848763437ca855e
joaoporto27 <joaovitorportosales46@gmail.com>
2025-04-10 16:18:05 -0300

    Feat: create more files

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 //const userRoutes = require("./src/routes/userRoutes");
+//const postsRouter = require("./src/routes/postsRouter");
+//const regionsRouter = require("./src/routes/regionsRouter");
+//const commentsRouter = require("./src/routes/commentsRouter");
+//const newsRouter = require("./src/routes/newsRouter")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 //app.use("/api/user", userRoutes);
+//app.use("/api/posts", postsRouter);
+//app.use("/api/regions", regionsRouter);
+//app.use("/api/comments", commentsRouter);
+//app.use("/api/news", newsRouter)
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
