afc45ca8d0a435e9175ed2d76c3f873924a5db96
unknown <lauraviolla.tds1.senai@gmail.com>
2025-04-17 14:02:10 -0300

    feat: add inserts from users

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -25,6 +25,7 @@ CREATE TABLE posts (
 -- Tabela de COMMENTS
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
+    post_id INTEGER REFERENCES posts(id) ON CASCADE,
     user_id INTEGER REFERENCES users(id) ON CASCADE,
     comment TEXT
 );
@@ -47,3 +48,12 @@ CREATE TABLE news (
     text TEXT NOT NULL,
     URL VARCHAR(255)
 );
+
+-- INSERTS de USERS
+INSERT INTO users (name, email, city, state, password) VALUES
+('Amanda Gomes Mechi', 'amanda.mechi@aluno.senai.br', 'Campinas', 'SP', '123456'),
+('Andre Lucca Santos', 'andre.l.santos45@aluno.senai.br', 'Campinas', 'SP', '234561'),
+('Flavia Regina Alexandre Mendes', 'andre.l.santos45@aluno.senai.br', 'Campinas', 'SP', '345612'),
+('Giovanna Caron de Barros', 'giovanna.c.barros@aluno.senai.br', 'Valinhos', 'SP', '456123'),
+('Isabella Borin de Moraes Rosa', 'isabella.b.rosa6@aluno.senai.br', 'Valinhos', 'SP', '567234'),
+('João Vitor Porto Sales', 'joao.v.sales15@aluno.senai.br', 'Campinas', 'SP', '678345');
