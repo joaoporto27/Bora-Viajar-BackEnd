4749fe2df136b91b91a2291b0a518f21da5f96c0
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-05-13 11:49:29 -0300

    feat: create add fix pdf/csv

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -16,54 +16,35 @@ const exportUserPDF = async (req, res) => {
         doc.pipe(res);
 
         // Título
-        doc.fontSize(20).font("Helvetica-Bold").text("Relatório de Usuários", { align: "center" });
-        doc.moveDown();
+        doc.fontSize(20).text("Relatório de Usuários", { align: "center" });
+        doc.moveDown(); 
 
         // Cabeçalho da Tabela
-        const tableTop = 130;
-        const rowHeight = 30;
-        const columnWidths = [50, 150, 200, 100, 100, 100];
-        const columnPositions = [50, 100, 250, 470, 580, 650];
+        const tableTop = 130; 
+        const rowHeight = 25; 
+        const columnWidths = [200, 200, 200, 200, 200];
         let y = tableTop;
-
+        
         doc.fontSize(12).font("Helvetica-Bold");
-        doc.text("ID", columnPositions[0], y, { width: columnWidths[0], align: "left" });
-        doc.text("Name", columnPositions[1], y, { width: columnWidths[1], align: "left" });
-        doc.text("Email", columnPositions[2], y, { width: columnWidths[2], align: "left" });
-        doc.text("City", columnPositions[3], y, { width: columnWidths[3], align: "left" });
-        doc.text("State", columnPositions[4], y, { width: columnWidths[4], align: "left" });
-        doc.text("Type User", columnPositions[5], y, { width: columnWidths[5], align: "left" });
-        doc.moveTo(50, y + rowHeight - 5).lineTo(750, y + rowHeight - 5).stroke(); // Ajuste na largura e posição
-
+        doc.text("Nome", 50, y, { width: columnWidths[0], align: "left" });
+        doc.text("Email", 250, y, { width: columnWidths[1], align: "left" });
+        doc.text("Cidade", 450, y, { width: columnWidths[2], align: "left" });
+        doc.text("Estado", 550, y, { width: columnWidths[3], align: "left" });
+        doc.text("Tipo de usuário", 600, y, { width: columnWidths[4], align: "left" });
+        doc.moveTo(50, y + rowHeight - 5).lineTo(700, y + rowHeight - 5).stroke(); 
+        
         // Dados da Tabela
         doc.font("Helvetica");
         y += rowHeight;
         users.forEach((user) => {
-            if (y + rowHeight > 750) {
-                doc.addPage();
-                y = 50;
-
-                doc.fontSize(12).font("Helvetica-Bold");
-                doc.text("ID", columnPositions[0], y, { width: columnWidths[0], align: "left" });
-                doc.text("Name", columnPositions[1], y, { width: columnWidths[1], align: "left" });
-                doc.text("Email", columnPositions[2], y, { width: columnWidths[2], align: "left" });
-                doc.text("City", columnPositions[3], y, { width: columnWidths[3], align: "left" });
-                doc.text("State", columnPositions[4], y, { width: columnWidths[4], align: "left" });
-                doc.text("Type User", columnPositions[5], y, { width: columnWidths[5], align: "left" });
-                doc.moveTo(50, y + rowHeight - 5).lineTo(800, y + rowHeight - 5).stroke();
-                y += rowHeight;
-            }
-
-            doc.font("Helvetica");
-            doc.text(user.id, columnPositions[0], y, { width: columnWidths[0], align: "left" });
-            doc.text(user.name, columnPositions[1], y, { width: columnWidths[1], align: "left" });
-            doc.text(user.email, columnPositions[2], y, { width: columnWidths[2], align: "left" });
-            doc.text(user.city, columnPositions[3], y, { width: columnWidths[3], align: "left" });
-            doc.text(user.state, columnPositions[4], y, { width: columnWidths[4], align: "left" });
-            doc.text(user.type_user, columnPositions[5], y, { width: columnWidths[5], align: "left" });
+            doc.text(user.name, 50, y, { width: columnWidths[0], align: "left" });
+            doc.text(user.email, 250, y, { width: columnWidths[1], align: "left" });
+            doc.text(user.city, 450, y, { width: columnWidths[2], align: "left" });
+            doc.text(user.state, 550, y, { width: columnWidths[3], align: "left" });
+            doc.text(user.type_user, 600, y, { width: columnWidths[4], align: "left" });
             y += rowHeight;
-
-            doc.moveTo(50, y - 2).lineTo(750, y - 2).stroke();
+        
+            doc.moveTo(50, y - 5).lineTo(700, y - 5).stroke();
         });
 
         doc.end();
@@ -74,51 +55,47 @@ const exportUserPDF = async (req, res) => {
 };
 
 // Função para gerar o PDF de posts
-const exportPotsPDF = async (req, res) => {
+const exportPostPDF = async (req, res) => {
     try {
         const posts = await postModel.getPosts();
 
         res.setHeader("Content-Type", "application/pdf");
         res.setHeader("Content-Disposition", "inline; filename=posts.pdf");
 
-        const doc = new PDFDocument();
+        const doc = new PDFDocument({ margin: 50, layout: 'landscape' });
         doc.pipe(res);
 
         // Título
-        doc.fontSize(20).text("Relatório de Posts", { align: "center" });
+        doc.fontSize(20).text("Relatório de posts", { align: "center" });
         doc.moveDown(); 
 
         // Cabeçalho da Tabela
-        const tableTop = 150;
-        const rowHeight = 20;
-        const rowHeightTable = 130;
-        const columnWidths = [50, 100, 150, 100, 100];
+        const tableTop = 130; 
+        const rowHeight = 25; 
+        const columnWidths = [200, 300, 200];
         let y = tableTop;
-
+        
         doc.fontSize(12).font("Helvetica-Bold");
-        doc.text("Id", 50, y, { width: columnWidths[0], align: "left" });
-        doc.text("Image", 100, y, { width: columnWidths[1], align: "left" });
-        doc.text("Description", 220, y, { width: columnWidths[2], align: "left" });
-        doc.text("Add Person", 400, y, { width: columnWidths[3], align: "left" });
-        doc.text("Localization", 500, y, { width: columnWidths[4], align: "left" });
-        doc.moveTo(50, y + rowHeight - 5).lineTo(600, y + rowHeight - 5).stroke(); 
-
+        doc.text("Usuário", 50, y, { width: columnWidths[0], align: "left" });
+        doc.text("Descrição", 250, y, { width: columnWidths[1], align: "left" });
+        doc.text("Etiqueta", 600, y, { width: columnWidths[2], align: "left" });
+        doc.moveTo(50, y + rowHeight - 5).lineTo(700, y + rowHeight - 5).stroke(); 
+        
         // Dados da Tabela
         doc.font("Helvetica");
         y += rowHeight;
         posts.forEach((post) => {
-            doc.text(post.id, 50, y, { width: columnWidths[0], align: "left" });
-            doc.text(post.image, 100, y, { width: columnWidths[1], align: "left" });
-            doc.text(post.description, 220, y, { width: columnWidths[2], align: "left" });
-            doc.text(post.add_person, 400, y, { width: columnWidths[3], align: "left" });
-            doc.text(post.localization, 500, y, { width: columnWidths[4], align: "left" });
-            y += rowHeightTable;
-            
-            doc.moveTo(50, y - 5).lineTo(600, y - 5).stroke();
+            doc.text(post.usuario, 50, y, { width: columnWidths[0], align: "left" });
+            doc.text(post.description, 250, y, { width: columnWidths[1], align: "left" });
+            doc.text(post.tag, 600, y, { width: columnWidths[2], align: "left" });
+            y += rowHeight;
+        
+            doc.moveTo(50, y - 5).lineTo(700, y - 5).stroke();
         });
 
         doc.end();
     } catch (error) {
+        console.error("Erro ao gerar o PDF:", error);
         res.status(500).json({ message: "Erro ao gerar o PDF" });
     }
 };
@@ -147,4 +124,28 @@ const exportUserCSV = async (req, res) => {
     }
 };
 
-module.exports = {  exportUserPDF, exportPotsPDF, exportUserCSV };
+const exportPostCSV = async (req, res) => {
+    try {
+        const posts = await postModel.getPosts();
+
+        res.setHeader("Content-Disposition", "attachment; filename=posts.csv");
+        res.setHeader("Content-Type", "text-csv");
+
+        const csvStream = format({ headers: true });
+        csvStream.pipe(res);
+
+        posts.forEach((user) => {
+            csvStream.write({
+                Usuário: user.usuario,
+                Descrição: user.description,
+                Etiqueta: user.tag
+            });
+        });
+
+        csvStream.end();
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao gerar o CSV" });
+    }
+};
+
+module.exports = {  exportUserPDF, exportPostPDF, exportUserCSV, exportPostCSV };
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const reportController = require("./../controllers/reportController");
 
 //Rota para gerar CSV
-router.get("/report/csv", reportController.exportUserCSV);
+router.get("/report/users/csv", reportController.exportUserCSV);
+router.get("/report/posts/csv", reportController.exportPostCSV);
 
 //Rota para gerar PDF
 router.get("/report/users/pdf", reportController.exportUserPDF);
-router.get("/report/posts/pdf", reportController.exportPotsPDF)
+router.get("/report/posts/pdf", reportController.exportPostPDF)
 
 module.exports = router;
